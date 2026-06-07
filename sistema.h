379373e9ea64080fab9bf74906534eac43a517a4
Larissa Lira <larissamnlira@gmail.com>
2026-06-07 18:31:51 -0300

    Menu e switch/case para o menu

--- sistema.h
+++ sistema.h
@@ -19,13 +19,57 @@ int main() {
     FILE *arquivo;
     int opcao;
 
-    // Abre o arquivo binário para leitura e escrita
     arquivo = fopen("contas.dat", "rb+");
     if (arquivo == NULL) {
-        // Se não existir, cria um novo arquivo binário
         arquivo = fopen("contas.dat", "wb+");
         if (arquivo == NULL) {
             printf("Erro ao criar o arquivo.\n");
             return 1;
         }
     }
+
+    do {
+        printf("< SISTEMA DE MANUTENÇÃO DE CONTAS >\n");
+        printf("1. Cadastrar um novo cliente em posição específica\n");
+        printf("2. Consultar um cliente pelo número da conta\n");
+        printf("3. Atualizar o saldo de um cliente\n");
+        printf("4. Encerrar conta (remover cliente)\n");
+        printf("5. Listar todos os clientes\n");
+        printf("6. Restaurar leitura do início (rewind) e listar\n");
+        printf("7. Encerrar\n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                cadastrar(arquivo);
+                break;
+            case 2:
+                consultar(arquivo);
+                break;
+            case 3:
+                atualizar(arquivo);
+                break;
+            case 4:
+                encerrar(arquivo);
+                break;
+            case 5:
+                listar(arquivo);
+                break;
+            case 6:
+                printf("\nExecutando rewind()...\n");
+                rewind(arquivo);
+                listar(arquivo); 
+                break;
+            case 7:
+                printf("Encerrando o sistema...\n");
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+        }
+    } 
+    
+    while (opcao != 7);
+    fclose(arquivo);
+    return 0;
+}
